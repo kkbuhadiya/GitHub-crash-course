b6b79aa968feb6797284a1669b89f0802650c1b2
Keshav Buhadiya <kkbuhadiya@gmail.com>
2024-02-27 10:00:04 +0530

    final update

--- index.js
+++ index.js
@@ -15,3 +15,26 @@ class subNameClass extends Myname {
 
 const userName = new subNameClass();
 console.log( userName.subName());
+
+// comput 
+const bar = {
+    foo0: function() {
+        return 0;
+    },
+    foo1:()=>{
+        return 1;
+    },
+    ["foo"+2](){
+        return 2;
+    },
+    foo3(){
+        return 3;
+    }
+
+}
+
+console.log(bar);
+console.log(bar.foo0());
+console.log(bar.foo1());
+console.log(bar.foo2());
+console.log(bar.foo3());
